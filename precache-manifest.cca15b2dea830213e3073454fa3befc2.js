2897a37a4de7d057d87691edc4fb9c43c08c9fbc
Alen Grbic <alengrbic90@gmail.com>
2020-08-09 01:16:00 +0100

    Updates

---
+++ precache-manifest.cca15b2dea830213e3073454fa3befc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38622f0ffd87718e947da3abd5167dbe",
+    "url": "/safe-check/index.html"
+  },
+  {
+    "revision": "0e8b441fd89d1982fb02",
+    "url": "/safe-check/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4418eb5f1c498ff15709",
+    "url": "/safe-check/static/js/2.3ffac951.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/safe-check/static/js/2.3ffac951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8b441fd89d1982fb02",
+    "url": "/safe-check/static/js/main.9fbad9a6.chunk.js"
+  },
+  {
+    "revision": "1ce1b58385ac053d2c0c",
+    "url": "/safe-check/static/js/runtime-main.bb89c4ec.js"
+  }
+]);
